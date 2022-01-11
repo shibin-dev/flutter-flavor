a747b89e5e56a0d4e83d1f2a5f8d4d05d588c40c
“shibin-dev” <shibinflutter@gmail.com>
2022-01-11 14:20:08 +0530

    add dev flavor to iOS

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Flavors'),
+      home: const MyHomePage(title: 'Flutter Flavor'),
     );
   }
 }
